5800c0dd057a9fe408d0f66ee81fd4033fb897aa
Ubuntu <ubuntu@t6.lxd>
2021-04-17 10:03:03 +0000

    Including api-examples

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # IB-Examples
-Collection of working skripts
 
-(in preparation)
+Collection of working scripts using the Interactive Brokers TWS API through  **_IB-Ruby_** 
+
+## Setup
+
+Its super easy to setup **_IB-Ruby_** in a _Lxc-Container_.
+
+* Require an Demo Account
+* Clone [IB-Container](https://github.com/ib-ruby/ib-container) and install one container on your hardware.
+* Enter the container  (use `lxc open _container-name_`)
+* Clone this respiratory
+* Run the install-script in /setup
+
+enjoy
+
+## Console and Gateway
+
+For interactive experiments, `console` and `gateway` scripts are included in the bin-directory
+
+
+## IB-Api-Scripts
+
+The examples in the `api`-directory just collect data from the _TWS_. They can be safely run outside 
+a demo account. 
+
+
+(to be continued)
---
+++ setup/install_gems.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+BASEDIR=~
+EXAMPLES_DIR=$BASEDIR/ib-examples
+git_archives=(ib-api ib-symbols ib-extensions)
+
+clone_git(){ 
+	if [ ` ls | grep $1 | wc -l`  -eq 0 ] ; then
+		git clone https://github.com/ib-ruby/$1 
+  else
+		echo "skipping  cloning $1, already present"
+	fi
+}
+
+
+make_gemfile(){
+	echo "
+
+# frozen_string_literal: true
+
+ source \"https://rubygems.org\"
+
+ #git_source(:github) {|repo_name| \"https://github.com/#{repo_name}\" }
+ gem \"ib-api\" , path: \"${BASEDIR}/ib-api/\"
+ gem \"ib-symbols\", path: \"${BASEDIR}/ib-symbols/\"
+ gem \"ib-extensions\", path: \"${BASEDIR}/ib-extensions/\"
+ gem \"pastel\"
+ gem \"dry-core\"
+ gem \"rspec\"
+ gem \"rspec-its\"
+ gem \"rspec-collection_matchers\"
+ gem \"guard\"
+ gem \"guard-rspec\"
+ gem \"standard\", group: [:development, :test]
+
+" > Gemfile
+}
+cd $BASEDIR
+for archive in ${git_archives[*]} ; do 
+	clone_git ${archive}
+done
+
+cd $EXAMPLES_DIR
+if [ ` ls | grep Gemfile | wc -l`  -eq 0 ] ; then
+	make_gemfile
+
+	bundle install
+	bundle update
+
+	echo "-----------------------------------------"
+	echo " setup finished                          "
+	echo ""
+	echo " you may now use the ib-ruby suite       "
+else
+	bundle update
+	echo "-----------------------------------------"
+	echo " setup updated                           "
+	echo ""
+fi
+
+
+cd $EXAMPLES_DIR
